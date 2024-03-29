ed562212e18fc3f5d6cfaaa331591b4f3314926f
kostiukevechdima <kostiukevechdima@gmail.com>
2024-03-29 20:12:55 +0200

    commit-1

---
+++ js/task-2.js
@@ -0,0 +1,30 @@
+class Storage {
+  #items = [];
+
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const indexOfItem = this.#items.indexOf(itemToRemove);
+    if (indexOfItem !== -1) {
+      this.#items.splice(indexOfItem, 1);
+    }
+  }
+}
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
