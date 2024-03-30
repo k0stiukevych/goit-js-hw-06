cc497bbd25499a5ae64afc7614c63804c8902424
kostiukevechdima <kostiukevechdima@gmail.com>
2024-03-30 10:47:31 +0200

    commit-2

--- js/task-2.js
+++ js/task-2.js
@@ -21,7 +21,6 @@ class Storage {
   }
 }
 
-
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem("Droid");
